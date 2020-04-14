0c352922325df7a7c382a698643685b3d6bb9718
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-14 21:56:09 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index do |name, index|
+    puts "#{index + 1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  true_false_arr = [];
+  calls.each do |call|
+    true_false_arr << (call.length > 4 ? true : false);
+  end
+  true_false_arr.include?(true)
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foods.collect do |food|
+    cheese_types.include?(food) ? (return food) : nil
+  end
+  (cheese_types & foods).empty? == true ? nil : false;
 end
